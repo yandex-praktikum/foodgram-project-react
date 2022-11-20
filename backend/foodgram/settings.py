95f7983e65788b5a84c6038ea13379eee1d8f638
Igor <102155826+igorentiy@users.noreply.github.com>
2022-11-20 20:07:57 +0300

    final? version

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -27,7 +27,6 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'django_filters',
-    
 ]
 
 MIDDLEWARE = [
--- backend/users/admin.py
+++ backend/users/admin.py
@@ -3,7 +3,6 @@ from django.contrib import admin
 from .models import User
 
 
-
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     list_display = (
