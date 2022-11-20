334ae592d0b4c56dc0aa3da593c8d6404a559c5b
Igor <102155826+igorentiy@users.noreply.github.com>
2022-11-20 19:52:16 +0300

    final? version

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -1,12 +1,16 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SECRET_KEY = 'wv3@s#zy@nit&0v76z&7o^pl=ogvqtc3xmf1_*g=#yeigkto(='
+SECRET_KEY = os.getenv('SECRET_KEY', default='wv3@s#zy@nit&0v76z&7o^pl=ogvqtc3xmf1_*g=#yeigkto(=')
 
-DEBUG = True
+DEBUG = os.getenv('DEBUG', default='True') == 'True'
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = os.environ.get(
+    'ALLOWED_HOSTS', default='localhost').split(', ')
 
 
 INSTALLED_APPS = [
@@ -60,33 +64,33 @@ WSGI_APPLICATION = 'foodgram.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 #DATABASES = {
 #    'default': {
-#        'ENGINE': os.getenv(
-#            'DB_ENGINE', default='django.db.backends.postgresql'),
-#        'NAME': os.getenv(
-#            'POSTGRES_DB',
-#            default='postgres'),
-#        'USER': os.getenv(
-#            'POSTGRES_USER',
-#            default='postgres'),
-#        'PASSWORD': os.getenv(
-#            'POSTGRES_PASSWORD',
-#            default='postgres'),
-#        'HOST': os.getenv(
-#            'DB_HOST',
-#            default='db'),
-#        'PORT': os.getenv(
-#            'DB_PORT',
-#            default='5432'),
-#    }}
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
+
+DATABASES = {
+    'default': {
+        'ENGINE': os.getenv(
+            'DB_ENGINE', default='django.db.backends.postgresql'),
+        'NAME': os.getenv(
+            'POSTGRES_DB',
+            default='postgres'),
+        'USER': os.getenv(
+            'POSTGRES_USER',
+            default='postgres'),
+        'PASSWORD': os.getenv(
+            'POSTGRES_PASSWORD',
+            default='postgres'),
+        'HOST': os.getenv(
+            'DB_HOST',
+            default='db'),
+        'PORT': os.getenv(
+            'DB_PORT',
+            default='5432'),
+    }}
 
 
 AUTH_PASSWORD_VALIDATORS = [
