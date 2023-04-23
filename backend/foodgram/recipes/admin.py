440addcfbd1eca18450d653f3877cfecd248d924
Владимир Кошелев <vladimirkoshelev3@gmail.com>
2023-04-23 20:18:58 +0700

    create recipes.models Tag, create recipes.admin ingredient tag

--- backend/foodgram/recipes/admin.py
+++ backend/foodgram/recipes/admin.py
@@ -1,3 +1,23 @@
-from django.contrib import admin
+from django.contrib.admin import ModelAdmin, site, StackedInline
 
-# Register your models here.
+from recipes.models import (Ingredient,
+                            Tag,
+                            )
+                            
+
+class IngidientAdmin(ModelAdmin):
+    list_display = ('id', 'name', 'measurement_unit')
+    list_editable = ('measurement_unit',)
+    search_fields = ('name',)
+    list_filter = ('name',)
+
+
+class TagAdmin(ModelAdmin):
+    list_display = ('id', 'name', 'color', 'slug')
+    list_editable = ('color', 'slug')
+    search_fields = ('name',)
+    list_filter = ('slug',)
+
+
+site.register(Ingredient, IngidientAdmin)
+site.register(Tag, TagAdmin)
--- backend/foodgram/recipes/models.py
+++ backend/foodgram/recipes/models.py
@@ -1,4 +1,4 @@
-from django.db.models import CharField, Model
+from django.db.models import CharField, Model, SlugField
 
 
 class Ingredient(Model):
@@ -17,6 +17,37 @@ class Ingredient(Model):
         ordering = ('id',)
         verbose_name = 'Ингредиент'
         verbose_name_plural = 'Ингредиенты'
+
+    def __str__(self):
+        return {self.name}
+
+
+class Tag(Model):
+    name = CharField(
+        verbose_name='Название тега',
+        help_text='Введите название тега',
+        max_length=200,
+        unique=True
+    )
+    color = CharField(
+        verbose_name='Цвет в НЕХ',
+        help_text='Выбирите цвет',
+        max_length=7,
+        unique=True,
+        blank=True,
+        null=True
+    )
+    slug = SlugField(
+        verbose_name='Слаг тега',
+        help_text='Введите слаг тега',
+        max_length=200,
+        unique=True
+    )
+
+    class Meta:
+        ordering = ('id',)
+        verbose_name = 'Тег'
+        verbose_name_plural = 'Теги'
 
     def __str__(self):
         return {self.name}
