c77a16bc2c0218ab9ad90feebd077c4a31258388
Владимир Кошелев <vladimirkoshelev3@gmail.com>
2023-04-23 20:03:25 +0700

    create recipes.models ingredient

--- backend/foodgram/recipes/models.py
+++ backend/foodgram/recipes/models.py
@@ -1,3 +1,22 @@
-from django.db import models
+from django.db.models import CharField, Model
 
-# Create your models here.
+
+class Ingredient(Model):
+    name = CharField(
+        verbose_name='Название ингредиента',
+        help_text='Введите ингредиент',
+        max_length=200
+    )
+    measurement_unit = CharField(
+        verbose_name='Единица измерения',
+        help_text='Введите единицу измерения',
+        max_length=200
+    )
+
+    class Meta:
+        ordering = ('id',)
+        verbose_name = 'Ингредиент'
+        verbose_name_plural = 'Ингредиенты'
+
+    def __str__(self):
+        return {self.name}
