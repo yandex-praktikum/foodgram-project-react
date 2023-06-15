343c4b957ac8a480f3d5069b0f35237e49259c66
Михаил Приселков <mikhail-upside@yandex.ru>
2023-06-16 00:31:58 +0300

    Add token url

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path, include
-from rest_framework.routers import SimpleRouter
+from rest_framework.routers import DefaultRouter
+from api.views import UserViewSet, token
 
-from api.views import UserViewSet
+app_name = 'api'
 
-router = SimpleRouter()
+router = DefaultRouter()
 
 router.register('users', UserViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('auth/token/login/', token, name='login'),
 ]
