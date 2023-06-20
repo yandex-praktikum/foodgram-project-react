c9c46fd445f3dc98e98540053114a344980a53e0
Олег Дядьков <dyadkov@gmail.com>
2023-06-20 14:35:39 +0500

    Изменил фильтры на ChoiceFilter

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -4,21 +4,38 @@ from recipes.models import Recipes
 
 
 class RecipesFilter(rest_framework.FilterSet):
-    is_favorited = rest_framework.BooleanFilter(method='filter_is_favorited')
-    is_in_shopping_cart = rest_framework.BooleanFilter(
-        method='filter_is_in_shopping_cart')
+    IN_SHOPPING_CART_CHOICES = (
+        (1, 'In shopping cart'),
+        (0, 'Not in shopping cart'),
+    )
+
+    IS_FAVORITES_CHOICES = (
+        (1, 'Is favorites'),
+        (0, 'Not is favorites'),
+    )
+
+    is_favorited = rest_framework.ChoiceFilter(
+        choices=IS_FAVORITES_CHOICES,
+        method='filter_is_favorited',
+        label='Is Favorited'
+    )
+    is_in_shopping_cart = rest_framework.ChoiceFilter(
+        choices=IN_SHOPPING_CART_CHOICES,
+        method='filter_is_in_shopping_cart',
+        label='Is in shopping cart'
+    )
 
     def filter_is_favorited(self, queryset, name, value):
         user = self.request.user
         if user.is_authenticated:
-            if value:
+            if value == 1:
                 queryset = queryset.filter(favorite__user=user)
         return queryset
 
     def filter_is_in_shopping_cart(self, queryset, name, value):
         user = self.request.user
         if user.is_authenticated:
-            if value:
+            if value == 1:
                 queryset = queryset.filter(purchase__user=user)
         return queryset
     
