471aa41c3b182212ddc3b93784a9858b50937687
Михаил Приселков <mikhail-upside@yandex.ru>
2023-06-20 18:49:36 +0300

    Add users model

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,52 +1,51 @@
+from djoser.serializers import UserCreateSerializer, UserSerializer
 from rest_framework import serializers
 from users.models import User, Subscribe
-# from django.contrib.auth import authenticate
-from django.shortcuts import get_object_or_404
 
 
-class TokenSerializer(serializers.Serializer):
-    """Сериализатор для получения токена."""
-    password = serializers.CharField(
-        required=True,
-        max_length=150,
-    )
-    email = serializers.EmailField(
-        required=True,
-        max_length=254
-    )
-
-    def validate(self, data):
-        email = data.get('email')
-        password = data.get('password')
-        user = get_object_or_404(User, email=email)
-        if not user.check_password(password):
-            raise serializers.ValidationError('Неверный логин или пароль')
-        return user
+class CreateUserSerializer(UserCreateSerializer):
+    class Meta:
+        model = User
+        fields = ('email', 'password', 'username', 'first_name', 'last_name',)
+        extra_kwargs = {'password': {'write_only': True}}
 
 
-class UserSerializer(serializers.ModelSerializer):
+class CustomUserSerializer(UserSerializer):
     is_subscribed = serializers.SerializerMethodField()
-    password = serializers.CharField(write_only=True)
 
     class Meta:
+        model = User
         fields = (
             'email',
+            'id',
             'username',
             'first_name',
             'last_name',
-            "password",
             'is_subscribed',
-            )
-        model = User
+        )
 
     def get_is_subscribed(self, obj):
-        user = self.context.get('request').user
-        if user.is_anonymous:
-            return False
-        return Subscribe.objects.filter(user=user, author=obj).exists()
+        user = self.context['request'].user
+        return Subscribe.objects.filter(
+            user=user, author=obj
+        ).exists() if user.is_authenticated else False
 
-    def create(self, validated_data):
-        user = User.objects.create_user(**validated_data)
-        user.set_password(validated_data['password'])
-        user.save()
-        return user
+
+class SubscribeSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField(source='author.id')
+    email = serializers.EmailField(source='author.email')
+    username = serializers.CharField(source='author.username')
+    first_name = serializers.CharField(source='author.first_name')
+    last_name = serializers.CharField(source='author.last_name')
+    is_subscribed = serializers.SerializerMethodField()
+
+    class Meta:
+        model = User
+        fields = (
+            'email',
+            'id',
+            'username',
+            'first_name',
+            'last_name',
+            'is_subscribed',
+            )
