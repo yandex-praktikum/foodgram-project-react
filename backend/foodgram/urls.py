dd22c110ec25a7d806566fca006218fd17e07624
Михаил Приселков <mikhail-upside@yandex.ru>
2023-06-22 18:35:18 +0300

    Add MEDIA_ROOT

--- backend/foodgram/urls.py
+++ backend/foodgram/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -8,3 +10,7 @@ urlpatterns = [
     path('api/', include('djoser.urls')),
     path('api/', include('djoser.urls.authtoken')),
 ]
+
+urlpatterns += static(
+      settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+    )
