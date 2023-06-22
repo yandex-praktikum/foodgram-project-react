846bf54d27f3a6ecdba0f1bfa137babaf2b83b49
Михаил Приселков <mikhail-upside@yandex.ru>
2023-06-22 15:49:36 +0300

    Add router ingredients

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,11 +1,12 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import UsersViewSet, TagViewSet
+from .views import UsersViewSet, TagViewSet, IngredientViewSet
 
 router = DefaultRouter()
 router.register('users', UsersViewSet, basename='users')
 router.register('tag', TagViewSet, basename='tag')
+router.register('ingredients', IngredientViewSet, basename='ingredients')
 
 urlpatterns = [
     path('', include(router.urls)),
