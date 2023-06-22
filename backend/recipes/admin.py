ec7175082a8070a5f5d59105582138a68112cf95
Михаил Приселков <mikhail-upside@yandex.ru>
2023-06-22 18:36:19 +0300

    Add class Recipes admin

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from recipes.models import Tag, Ingredient
+from recipes.models import Tag, Ingredient, Recipes
 from import_export import resources
 from import_export.admin import ImportExportModelAdmin
+from django.utils.safestring import mark_safe
 
 
 class TagAdmin(admin.ModelAdmin):
@@ -21,5 +22,24 @@ class IngredientAdmin(ImportExportModelAdmin):
     resource_class = IngredientResource
 
 
+class RecipesAdmin(admin.ModelAdmin):
+    fields = (
+        'ingredients',
+        'tags',
+        'image',
+        'preview',
+        'name',
+        'text',
+        'cooking_time',
+        )
+    readonly_fields = ('preview',)
+
+    def preview(self, obj):
+        return mark_safe(
+            f'<img src="{obj.image.url}" style="max-height: 200px;">'
+            )
+
+
 admin.site.register(Ingredient, IngredientAdmin)
 admin.site.register(Tag)
+admin.site.register(Recipes, RecipesAdmin)
