3733796250aa43df2232f2a3bf98115e1a93ed9e
BaronFAS <mikhail-upside@yandex.ru>
2023-07-08 07:29:59 +0000

    Bug fix

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -21,7 +21,7 @@ SECRET_KEY = os.getenv("SECRET_KEY", "")
 
 DEBUG = os.getenv("DEBUG", "false").lower() == "true"
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["158.160.38.73", '127.0.0.1', 'localhost', 'baronfel-foodgram.ddns.net']
 
 
 INSTALLED_APPS = [
@@ -70,13 +70,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = "foodgram.wsgi.application"
-
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
 
 
 DATABASES = {
