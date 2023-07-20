62df79e21b7e1172a02f7a98ad933286bd156d08
Elizaveta Strunnikova <liza.strunnikova@yandex.ru>
2023-07-20 21:33:51 +0300

    small fixes in settings

--- backend/api_foodgram/api_foodgram/settings.py
+++ backend/api_foodgram/api_foodgram/settings.py
@@ -23,6 +23,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'django_filters',
     'api',
     'users',
@@ -131,9 +132,9 @@ REST_FRAMEWORK = {
     # 'DEFAULT_PERMISSION_CLASSES': [
     #     '',
     # ],
-    # 'DEFAULT_AUTHENTICATION_CLASSES': (
-    #     '',
-    # ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 6,
 }
