c1fd98c0fca8de7a019f2fd28b3c4ec5b26ec0f2
Sergei Vinogradov <airtrecx4@gmail.com>
2023-11-07 22:31:19 +0300

    Деплой.0

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -9,9 +9,14 @@ from pathlib import Path
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SECRET_KEY = "django-insecure-051ek9e6-u)7oq=(bws@q0f1=vg$n66w81zlye2&mmue!is5ni"
+SECRET_KEY = os.getenv(
+    'SECRET_KEY',
+    default='django-insecure-051ek9e6-u)7oq=(bws@q0f1=vg$n66w81zlye2&mmue!is5ni'
+)
+# SECRET_KEY = "django-insecure-051ek9e6-u)7oq=(bws@q0f1=vg$n66w81zlye2&mmue!is5ni"
 
-DEBUG = True
+DEBUG = os.getenv('DEBUG', default='True')
+
 
 ALLOWED_HOSTS = []
 
@@ -69,8 +74,11 @@ WSGI_APPLICATION = "foodgram.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": os.getenv("DB_ENGINE", default="django.db.backends.postgresql"),
-        "NAME": os.getenv("DB_NAME", default="postgres"),
+        'ENGINE': os.getenv('DB_ENGINE', default='django.db.backends.sqlite3'),
+        # "ENGINE": os.getenv("DB_ENGINE", default="django.db.backends.postgresql"),
+        'NAME': os.getenv('DB_NAME',
+                          default=os.path.join(BASE_DIR, 'db.sqlite3')),
+        # "NAME": os.getenv("DB_NAME", default="postgres"),
         "USER": os.getenv("POSTGRES_USER", default="postgres"),
         "PASSWORD": os.getenv("POSTGRES_PASSWORD", default="postgres"),
         "HOST": os.getenv("DB_HOST", default="db"),
