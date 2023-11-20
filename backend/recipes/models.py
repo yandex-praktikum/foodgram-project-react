58fd04697b2d12796b9102a72002d867f35c203d
Sergei Vinogradov <airtrecx4@gmail.com>
2023-11-20 12:38:30 +0300

    initial commit. убрал лишнюю строку

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,7 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.core.validators import MinValueValidator
 from django.db import models
-
 from django.utils.translation import gettext_lazy as _
 
 
