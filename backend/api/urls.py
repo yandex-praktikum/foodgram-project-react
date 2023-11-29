e4686005a2d6a95dde9775e31ed24b831bda5fbf
Sergei Vinogradov <airtrecx4@gmail.com>
2023-11-29 12:59:26 +0300

    vse kok

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -20,19 +20,19 @@ router = DefaultRouter()
 router.register("ingredients", IngridientsViewSet)
 router.register("tags", TagsViewSet)
 router.register("recipes", RecipesViewSet)
-router.register(
-    r'recipes/(?P<recipe_id>\d+)/favorite', FavoriteRecipeViewSet,
-    basename='favorite')
+# router.register(
+    # r'recipes/(?P<recipe_id>\d+)/favorite', FavoriteRecipeViewSet,
+    # basename='favorite')
 
 urlpatterns = [
     path(
         "recipes/<int:id>/shopping_cart/",
         ShoppingCartViewSet.as_view({"post": "create", "delete": "destroy"})
     ),
-    #path(
-        #"recipes/<int:id>/favorite/",
-        #FavoriteRecipeViewSet.as_view({"post": "create", "delete": "destroy"})
-    #),
+    path(
+        "recipes/<int:id>/favorite/",
+        FavoriteRecipeViewSet.as_view({"post": "create", "delete": "destroy"})
+    ),
     path(
         "users/subscriptions/", SubscriptionsViewSet.as_view({"get": "list"})
     ),
