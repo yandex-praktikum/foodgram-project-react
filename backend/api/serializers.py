71e6919878248ce0a5f6cab89447806da82ca2fc
Sergei Vinogradov <airtrecx4@gmail.com>
2023-12-04 17:28:34 +0300

    mud.3

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -87,17 +87,18 @@ class RecipesSerializer(serializers.ModelSerializer):
 
     def get_is_favorited(self, obj):
         print((self.context.get('request').user.is_authenticated
-               and
-               Favorite.objects.filter(
+                and
+                Favorite.objects.filter(
                     user=self.context.get('request').user,
                     recipe=obj
-        ).exists()))
+               ).exists()
+              ))
         return (self.context.get('request').user.is_authenticated
                 and
                 Favorite.objects.filter(
                     user=self.context.get('request').user,
                     recipe=obj
-        ).exists())
+                ).exists())
 
     def get_is_in_shopping_cart(self, obj):
         return (self.context.get('request').user.is_authenticated
