ea84e03fbe8fc8f395b8e5e81a96534ca4f568cc
Sergei Vinogradov <airtrecx4@gmail.com>
2023-12-04 16:20:11 +0300

    B yc

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -13,8 +13,8 @@ SECRET_KEY = os.getenv(
 )
 
 DEBUG = os.getenv('DEBUG', default='True')
-ALLOWED_HOSTS = []
-# ALLOWED_HOSTS = ['130.193.53.39', 'devinse.ru']
+
+ALLOWED_HOSTS = ['130.193.53.39', 'devinse.ru']
 
 INSTALLED_APPS = [
     "django.contrib.admin",
